b7d4293451368e946a95cb06cd4f2b0245d9a7c3
billyyoung <billyisyoung@gmail.com>
2020-12-27 01:48:29 -0500

    Day 19: update Part 1 to lists, solve Part 2

--- day19.py
+++ day19.py
@@ -2,17 +2,15 @@ from collections import defaultdict
 import re
 from copy import deepcopy
 
-with open("day19.sample2") as infile:
+with open("day19.input") as infile:
     lines = [l for l in infile.read().split("\n") if l]
 
 rules = {}
-i = 0
-while True:
+for i in range(len(lines)):
     if ':' not in lines[i]:
         break
 
-    # parse rule:
-    # N: rule
+    # parse rule: "N: <rule>"
     rule_num, rule = lines[i].split(": ")
     if '"' in rule:
         parsed_rule = rule[1]  # "a"
@@ -23,45 +21,71 @@ while True:
             for r in rule_list
         ]
     rules[int(rule_num)] = parsed_rule
-    i += 1
 
 messages = lines[i:]
 
-def is_valid(msg, rules):
-    if consume(msg, rules, 0) == '':  # fully consumed
-        return True
-    return False
+def is_valid(msg, rules, debug=False):
+    # if any msg was fully consumed by a ruleset
+    return '' in consume(msg, rules, 0, 1, debug)
 
-def consume(msg, rules, rule_num):
+def consume(msg, rules, rule_num, level, debug):
     if msg == '':  # trying to consume with nothing left...
-        return 'fail'
+        return set()
 
     if rules[rule_num] in ['a', 'b']:
-        return msg[1:] if msg[0] == rules[rule_num] else 'fail'
+        return {msg[1:]} if msg[0] == rules[rule_num] else set()
+
+    # possible success paths after feeding 'msg' through the rule lists
+    possibilities = set()
 
     rule_list = rules[rule_num]
-    # if 2222 in rules and rule_num in [8, 11]:
-    #     import pdb;pdb.set_trace()
-
     # rule_list = [ [1, 2, 3, ...], [... ]
-    # rule = [1, 2, 3, ...]
-    for rule in rule_list:
-        test_msg = msg
 
+    for rule in rule_list:  # e.g. [1, 2, 3, ...]
+
+        # try 'msg' on this particular ruleset
+        test_msgs = {msg}
+
+        # see what possible consumptions there are
         success = True
-        for r in rule:
-            res = consume(test_msg, rules, r)
-            if res == test_msg or res == 'fail':  # rule failed, get out
+        for r in rule:  # e.g. 42 of [42, 11, 31]
+
+            temp = set()
+            for t in test_msgs:
+
+                if debug:
+                    print "%s [try] - [%s] -> %s" % (
+                        " " * level,
+                        ','.join([str(x) for x in rule]),
+                        test_msgs
+                    )
+
+                results = consume(t, rules, r, level+1, debug)
+                for res in results:
+                    temp.add(res)
+
+            # if none of the possibilities passed 't':
+            # then this entire rule is a failure, break early
+            if not temp:
                 success = False
                 break
 
-            else:  # rule worked, keep going with res
-                test_msg = res
+            # some possibilities passed 't', move them forward to the next 'r'
+            test_msgs = temp
 
-        if success:
-            return test_msg  # all r's succeeded, return consumed msg
+        if success:  # found some things that passed this ruleset
 
-    return msg  # nothing succeeded, this msg failed
+            if debug:
+                print "%s [result] - [%s] -> %s" % (
+                    " " * level,
+                    ','.join([str(x) for x in rule]),
+                    test_msgs
+                )
+
+            # test_msgs are the results of the consumption, move forward
+            possibilities = possibilities.union(test_msgs)
+
+    return possibilities
 
 count = 0
 for i in range(len(messages)):
@@ -70,12 +94,9 @@ for i in range(len(messages)):
     valid = is_valid(msg, rules)
     if valid:
         count += 1
-
 print count
-print
 
 # part 2
-rules[2222] = 1  # hack to flag part 2
 rules[8] = [[42, 8], [42]]
 rules[11] = [[42, 11, 31], [42, 31]]
 
@@ -83,10 +104,7 @@ count = 0
 for i in range(len(messages)):
     msg = messages[i]
 
-    # msg = 'babbbbaabbbbbabbbbbbaabaaabaaa'
     valid = is_valid(msg, rules)
     if valid:
         count += 1
-    print "%s - %s" % (msg, valid)  # debug
-
 print count
